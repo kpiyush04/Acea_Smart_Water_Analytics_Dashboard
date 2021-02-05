9b249e67bf0b16577eeabed72d951193c11b89a1
Piyush Kumar <04kpiyush@gmail.com>
2021-02-05 12:01:23 +0530

    web_app

--- R/server.R
+++ R/server.R
@@ -4,6 +4,14 @@ library(shinydashboard)
 Aquifer_Auser_1 <- read.csv("../data/Aquifer_Auser.csv")
 Aquifer_Doganella_1 <- read.csv("../data/Aquifer_Doganella.csv")
 Aquifer_Luco_1 <- read.csv("../data/Aquifer_Luco.csv")
+Aquifer_Petrignano_1 <- read.csv("../data/Aquifer_Petrignano.csv")
+Luke_Bilancino_1 <- read.csv("../data/Lake_Bilancino.csv")
+River_Arno_1 <- read.csv("../data/River_Arno.csv")
+Water_Spring_Amiata_1 <- read.csv("../data/Water_Spring_Amiata.csv")
+Water_Spring_Lupa_1 <- read.csv("../data/Water_Spring_Lupa.csv")
+Water_Spring_Madonna_di_Canneto_1 <- read.csv("../data/Water_Spring_Madonna_di_Canneto.csv")
+
+
 
 shinyServer <- function(input, output, session) {
   
@@ -12,16 +20,43 @@ shinyServer <- function(input, output, session) {
     Aquifer_Auser_1
   })
   
-  
-  #---------- Aquifer_Doganella data ---------------
+  # ---------- Aquifer_Doganella data ---------------
   output$Aquifer_Doganella_ <- renderDataTable({
     Aquifer_Doganella_1
   })
-  
-  
-  
-  # ----------- Aquifer_Luco data ------------------
+
+  # ---------- Aquifer_Luco data ------------------
   output$Aquifer_Luco_ <- renderDataTable({
     Aquifer_Luco_1
+  })
+   
+  # ---------- Aquifer_Petrignano data -------------
+  output$Aquifer_Petrignano_ <- renderDataTable({
+    Aquifer_Petrignano_1
+  })
+   
+  # ---------- Luke_Bilancino data -------------
+  output$Luke_Bilancino_ <- renderDataTable({
+    Luke_Bilancino_1
+  })
+   
+  # ---------- River_Arno data -------------
+  output$River_Arno_ <- renderDataTable({
+    River_Arno_1
+  })
+   
+  # ---------- Water_Spring_Amiata_ data -------------
+  output$Water_Spring_Amiata_ <- renderDataTable({
+    Water_Spring_Amiata_1
+  })
+   
+  # ---------- Water_Spring_Lupa_ data -------------
+  output$Water_Spring_Lupa_ <- renderDataTable({
+    Water_Spring_Lupa_1
+  })
+   
+  # ---------- Water_Spring_Madonna_di_Canneto data -------------
+  output$Water_Spring_Madonna_di_Canneto_<- renderDataTable({
+    Water_Spring_Madonna_di_Canneto_1
   })
 }
--- R/ui.R
+++ R/ui.R
@@ -4,49 +4,81 @@ library(shinydashboard)
 shinyUI(
   dashboardPage(
     
-    # ------------  DashBoard Header ---------------
+    # ---------------- DashBoard Header ---------------
     dashboardHeader(
       title = "Acea Smart Water Analytics", titleWidth = 280
       ),
     
     
-    # ------------  DashBoard Sidebar ---------------
+    # ---------------- DashBoard Sidebar ---------------
     
     dashboardSidebar(
       sidebarMenu(
         
-        # --------------- About ------------------
+        # ------------ About ------------------
         menuItem(
           text = "ABOUT", tabName = "about", icon = icon("clipboard")
           ),
         
-        # ------------- Data -----------------------
+        # ------------ Data -----------------------
         menuItem(
           "DATA",
           
-          # ------------ Sub menu Aquifer_Auser --------------
+          # ---------- Sub menu Aquifer_Auser --------------
           menuSubItem(
             "Aquifer_Auser.CSV", tabName = "Aquifer_Auser"
           ),
           
-          # ---------- sub menu Aquifer_Doganella ---------------
+          # ---------- Sub menu Aquifer_Doganella ---------------
           menuSubItem(
             "Aquifer_Doganella.CSV", tabName = "Aquifer_Doganella" 
           ),
+          
+          # ---------- Sub menu Aquifer_Luco -------------
           menuSubItem(
             "Aquifer_Luco.CSV", tabName = "Aquifer_Luco"
+          ),
+           
+          # ---------- Sub menu Aquifer_Petrignano ------------
+          menuSubItem(
+            "Aquifer_Petrignano.CSV", tabName = "Aquifer_Petrignano"
+          ),
+          
+          # ---------- Sub menu Lake_Bilancino --------------
+          menuSubItem(
+            "Luke_Bilancino.CSV", tabName = "Luke_Bilancino"
+          ),
+           
+          # ---------- Sub menu River_Arno ---------------
+          menuSubItem(
+            "River_Arno.CSV", tabName = "River_Arno"
+          ),
+           
+          # ---------- Sub menu water_Spring_Amiata.csv ------------
+          menuSubItem(
+            "Water_Spring_Amiata.CSV", tabName = "Water_Spring_Amiata"
+          ),
+           
+          # ---------- Sub menu Water_Spring_Lupa ---------------
+          menuSubItem(
+            "Water_Spring_Lupa.CSV", tabName = "Water_Spring_Amiata"
+          ), 
+           
+          # ---------- Sub menu Water_Spring_Madonna_di_Canneto -------
+          menuSubItem(
+            "Water_Spring_Madonna_di_Canneto.CSV", tabName = "Water_Spring_Madonna_di_Canneto"
           )
           )
       )
       ),
     
     
-    # ------------  DashBoard Body ---------------
+    # ---------------- DashBoard Body ---------------
     
     dashboardBody(
       tabItems(
         
-        # ----------- Tab About ------------------
+        # ------------ Tab About ------------------
         tabItem(
           tabName = "about", p("The Acea Group is one of the leading Italian multiutility operators. 
                                Listed on the Italian Stock Exchange since 1999, 
@@ -69,21 +101,51 @@ shinyUI(
                                )
         ),
         
-        # --------------- Tab Aquifer_AUser -------------------
+        # ------------ Tab Aquifer_AUser -------------------
         tabItem(
           tabName = "Aquifer_Auser", dataTableOutput("Aquifer_Auser_")
         ),
         
         
-        # --------------- Tab Aquifer_Doganella ----------------
+        # ------------ Tab Aquifer_Doganella ----------------
         tabItem(
           tabName = "Aquifer_Doganella", dataTableOutput("Aquifer_Doganella_")
         ),
         
         
-        # --------------- Tab Aquifer_Luco ----------------------
+        # ------------ Tab Aquifer_Luco ----------------------
         tabItem(
           tabName = "Aquifer_Luco", dataTableOutput("Aquifer_Luco_")
+        ),
+        
+        # ------------ Tab Aquifer_Petrignano ------------
+        tabItem(
+          tabName = "Aquifer_Petrignano", dataTableOutput("Aquifer_Petrignano_")
+        ),
+         
+        # ------------ Sub menu Lake_Bilancino --------------
+        tabItem(
+          tabName = "Luke_Bilancino", dataTableOutput("Luke_Bilancino_")
+        ),
+         
+        # ------------ Sub menu River_Arno ---------------
+        tabItem(
+          tabName = "River_Arno", dataTableOutput("River_Arno_")
+        ),
+        
+        # ------------ Sub menu water_Spring_Amiata.csv ------------
+        tabItem(
+          tabName = "Water_Spring_Amiata", dataTableOutput("Water_Spring_Amiata_")
+        ),
+         
+        # ------------ Sub menu Water_Spring_Lupa ---------------
+        tabItem(
+          tabName = "Water_Spring_Lupa", dataTableOutput("Water_Spring_Lupa_")
+        ),
+         
+        # ------------ Sub menu Water_Spring_Madonna_di_Canneto -------
+        tabItem(
+          tabName = "Water_Spring_Madonna_di_Canneto", dataTableOutput("Water_Spring_Madonna_di_Canneto_")
         )
       )
     )
